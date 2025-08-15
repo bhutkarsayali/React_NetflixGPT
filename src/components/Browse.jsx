2c4729bb227b07c234260574451406d4a97fe55f
Sayali <bhutkarsayali@gmail.com>
2025-08-16 00:21:22 +0530

    17. Moving logic to fetch data and update store to the custom hook so that component looks cleaner and using that hooks inside component

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,32 +1,8 @@
-import React, { useEffect } from "react";
+import useNowPlayingMovies from "../custom_hooks/useNowPlayingMovies";
 import Header from "./Header";
-import { TMDB_API_OPTIONS } from "../utils/constants";
-import { useDispatch } from "react-redux";
-import { addNowPlayingMovies } from "../utils/moviesSlice";
 
 const Browse = () => {
-  // add json.results to movie slide by dispatching an action
-  const dispatch = useDispatch();
-
-  const getNowPlayingMovies = async () => {
-    // Fetch data from TMDB API
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?page=1",
-      TMDB_API_OPTIONS
-    );
-    const json = await data.json();
-    // console.log(json);
-    console.log(json.results);
-
-    // Update store with fetched data
-    dispatch(addNowPlayingMovies(json.results));
-  };
-
-  useEffect(() => {
-    //needs to be called only once so kept in useEffect with empty []
-    // without [], you will see infinite API calls
-    getNowPlayingMovies();
-  }, []);
+  useNowPlayingMovies();
   return (
     <div>
       <Header />
---
+++ src/custom_hooks/useNowPlayingMovies.jsx
@@ -0,0 +1,31 @@
+import { useDispatch } from "react-redux";
+import { TMDB_API_OPTIONS } from "../utils/constants";
+import { addNowPlayingMovies } from "../utils/moviesSlice";
+import { useEffect } from "react";
+
+const useNowPlayingMovies = () => {
+  // add json.results to movie slide by dispatching an action
+  const dispatch = useDispatch();
+
+  const getNowPlayingMovies = async () => {
+    // Fetch data from TMDB API
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?page=1",
+      TMDB_API_OPTIONS
+    );
+    const json = await data.json();
+    // console.log(json);
+    console.log(json.results);
+
+    // Update store with fetched data
+    dispatch(addNowPlayingMovies(json.results));
+  };
+
+  useEffect(() => {
+    //needs to be called only once so kept in useEffect with empty []
+    // without [], you will see infinite API calls
+    getNowPlayingMovies();
+  }, []);
+};
+
+export default useNowPlayingMovies;
