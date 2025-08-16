65c9eea12cbfb44caff460bd1acad7b957c45ddf
Sayali <bhutkarsayali@gmail.com>
2025-08-17 01:31:44 +0530

    Removed consoles

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,7 +1,6 @@
 import MovieCard from "./MovieCard";
 
 const MovieList = ({ title, movies }) => {
-  console.log(movies);
   return (
     <div className="px-20 shadow-lg ">
       <h1 className="text-3xl pt-6 pb-2 text-white text-white font-semibold tracking-wide truncate drop-shadow-lg leading-tight">
